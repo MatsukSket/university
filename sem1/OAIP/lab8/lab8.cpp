f3a4a74ed9a1b884012c18b389ccd933438c2956
MatsukSket <zaharmatsukevich@gmail.com>
2024-11-20 10:53:05 +0300

    fix: oaip lab8

--- sem1/OAIP/lab8/lab8.cpp
+++ sem1/OAIP/lab8/lab8.cpp
@@ -2,15 +2,26 @@
 
 using namespace std;
 
-double my_func(int n){
+double my_rec_func(int n){
     if (n == 1)
         return 2/3.;
     else
-        return 1 / (n + my_func(n-1));
+        return 1 / (n + my_rec_func(n-1));
 }
 
+double my_func(int n){
+    double res;
+    for (int i = 1; i <= n; i++){
+        if (i == 1) 
+            res = 2/3.;
+        else
+            res = 1 / (i + res);
+    }
+    return res;
+}
 int main(){
     int n;  
     cout << "Enter n: ";    cin >> n;
-    cout << my_func(n) << endl;
+    cout << "Result of recursive func: " << my_rec_func(n) << endl;
+    cout << "Result of defoult func:   " << my_func(n) << endl;
 }
