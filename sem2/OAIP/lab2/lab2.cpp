42ce6ac986971e41c1c5141e9e0700aea0048cfb
MatsukSket <zaharmatsukevich@gmail.com>
2025-03-06 12:16:28 +0300

    update sem2 oaip lab2

--- sem2/OAIP/lab2/lab2.cpp
+++ sem2/OAIP/lab2/lab2.cpp
@@ -2,115 +2,189 @@
 #include <ctime>
 #include <cstdlib>
 
-
-using namespace std;
-
-struct Stack {
-    int data;
-    Stack *next;
-};
-
-Stack *myStack = NULL;
-
-bool isEmpty(Stack *stack)
-{
-    return stack == NULL;
-}
-
-int random()
-{
+int getRandom(){
     return rand() % 199 - 99;
 }
 
-void push(Stack **stack, int newValue) 
-{
-    Stack *link = new Stack;
-    link->data = newValue;
-    
-    link->next = *stack;
-    *stack = link;
-}
+class Stack{
+private:
+    class Node{
+    public:
+        int data;
+        Node *next = NULL;
+    };
+    Node *head = NULL;
 
-void view(Stack *beg)
-{   
-    if (isEmpty(myStack))
-        cout << "Stack is empty";
-    else {
-        Stack *curr = beg;
-        while(curr){
-            cout << curr->data << ' ';
-            curr = curr->next;
+    void copyFrom(const Stack &other)
+    {
+        if(!other.head){
+            head = NULL;
+            return;
+        }
+
+        head = new Node{other.head->data, NULL};
+        Node *currThis = head;
+        Node *currOther = other.head->next;
+
+        while (currOther){
+            currThis->next = new Node{currOther->data, NULL};
+            currThis = currThis->next;
+            currOther = currOther->next;
         }
     }
-    cout << endl;
-    return;
-}
+    
+public:
 
-void Del_All(Stack **curr)
-{
-    while(*curr) {
-        Stack *temp = *curr;
-        *curr = (*curr)->next;
+    Stack &operator=(const Stack &other){
+        if(this != &other){
+            deleteAll();
+            copyFrom(other);
+        }
+        return *this;
+    }
+
+    bool isEmpty() {
+        return head == NULL;
+    }
+
+    int top(){
+        return head->data;
+    }
+
+    void push(int new_value) 
+    {
+        Node *new_node = new Node;
+        new_node->data = new_value;
+        new_node->next = head;
+        head = new_node; 
+    }
+
+    void pop()
+    {
+        if(!head){
+            std::cout << "Stack is empty\n";
+            return;
+        }
+
+        Node *temp = head;
+        head = head->next;
         delete temp;
     }
-}
+
+    void print()
+    {
+        if(!head){
+            std::cout << "Stack is empty\n";
+            return;
+        }
+
+        Node *curr = head;
+        while(curr){
+            std::cout << curr->data << ' ';
+            curr = curr->next;
+        }
+        std::cout << '\n';
+    }
+
+    int findMax()
+    {
+        Node *curr = head;
+        int max_element = curr->data;
+        while(curr){
+            if (curr->data > max_element)
+                max_element = curr->data;
+            curr = curr->next;
+        }
+        
+        return max_element;
+    }
+
+    Stack copyToMax()
+    {
+        Stack temp_stack, reverse_stack;
+        Node *curr = head;
+        int max_element = findMax(); 
+
+        while(curr){
+            temp_stack.push(curr->data);
+            if(curr->data == max_element)
+                break;
+            curr = curr->next;
+        }
+
+        while(!temp_stack.isEmpty()){
+            reverse_stack.push(temp_stack.top());
+            temp_stack.pop();
+        }
+
+        reverse_stack.print();
+
+        return reverse_stack;
+    }
+
+    void deleteAll()
+    {
+        while (head) {
+            Node *temp = head;
+            head = head->next; 
+            delete temp;
+        }
+    }
+
+    ~Stack(){
+        deleteAll();
+    }
+};
 
 int main()
 {
-    int whatToDo, count;
-    while (true) {
-        cout << "1 - Create\n";
-        cout << "2 - Add\n";
-        cout << "3 - View\n";
-        cout << "4 - Delete\n";
-        cout << "5 - Task\n";
-        cout << "0 - Exit\n";
-        cin >> whatToDo;
-        
+    srand(static_cast<unsigned int>(time(0))); 
+
+    int whatToDo;
+    Stack myStack, taskStack;
+
+    std::cout << "1 - Add\n";
+    std::cout << "2 - View\n";
+    std::cout << "3 - Delete last\n";
+    std::cout << "4 - Delete all\n";
+    std::cout << "5 - Task\n";
+    std::cout << "0 - Exit\n";
+
+    bool stop = false;
+    while(!stop){
+        std::cout << "Enter the number of activity: ";
+        std::cin >> whatToDo;
+
         switch (whatToDo)
         {
-        // create
-        case 1: 
-            if (!isEmpty(myStack)){
-                cout << "Clear memory!\n";
-                break;
-            }
-
-            cout << "Input the quantity: ";   cin >> count;
-           
-            for (int i = 0; i < count; i++)
-                push(&myStack, random());
-
+        case 1:
+            myStack.push(getRandom());
             break;
-        // add
         case 2:
-            cout << "Input the quantity: ";   cin >> count;
-            
-            for (int i = 0; i < count; i++)
-                push(&myStack, random());
-
+            myStack.print();
             break;
-        // view
         case 3:
-            if (isEmpty(myStack)) {
-                cout << "Stack is empty\n";
+            myStack.pop();
+            break;
+        case 4:
+            myStack.deleteAll();
+            break;
+        case 5:
+            if(myStack.isEmpty()){
+                std::cout << "Stack is empty\n";
                 break;
             }
-            
-            view(myStack);
+
+            taskStack = myStack.copyToMax();
             break;
-        // delete stack
-        case 4:
-            Del_All(&myStack);
-            break;
-        // my task
-        case 5:
-            break;
-        // exit
         case 0:
-            Del_All(&myStack);
-            return 0;
+            stop = true;
+            break;
+        default:
+            std::cout << "Incorrect input. Try again\n";
+            break;
         }
     }
+
     return 0;
 }
